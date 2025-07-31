b31e48462dc84c0d806761627521a267ef25568f
udayveershaktawat <udayveershaktawat@gmail.com>
2025-07-31 23:55:41 +0530

    Otp controller completed

---
+++ controllers/Auth.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const optGenerator = require("otp-generator");
+
+
+
+// send OTP
+exports.sendOTP = async(req,res)=>{
+    try{
+        // fet h email from req.body
+        const {email} = req.body;
+
+        // check if user already exist
+        const checkUserPresent = await User.findOne({email});
+
+        // if user already exits then return res
+        if(checkUserPresent){
+            return res.status(401).json({
+                success:false,
+                message:"User already exists"
+            })
+        }
+
+        // generate otp
+
+        var otp = optGenerator.generate(6,{
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false,
+        });
+        console.log("OTP Generate",otp);
+
+        // check otp unique or not
+        let result = await OTP.findOne({otp:otp});
+
+        while(result){
+            
+         otp = optGenerator.generate(6,{
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false,
+        });
+
+        result = await OTP.findOne({otp:otp});
+
+        }
+
+        const otpPayload = {email,otp};
+
+        // create an entery for OTP
+        const otpBody = await OTP.create({otpPayload});
+        console.log(otpBody);
+        
+    
+
+
+    }
+    catch(error){
+         
+    }
+}

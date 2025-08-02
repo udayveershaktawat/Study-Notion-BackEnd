42b7ff94079bbefc70af1030fa040fb3160dc5b6
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-02 23:59:36 +0530

    course handler completed

--- controllers/Course.js
+++ controllers/Course.js
@@ -1,3 +1,26 @@
 const Course = require("../models/Course");
 const Tag = require("../models/Tag");
-const User = require("../models/User");
+const User = require("../models/User"); 
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
+// create course handler function
+exports.createCourse = async(req,res)=>{
+    try{
+        // fetch data
+        // validation
+        // 
+
+    }
+    catch(error){
+
+    }
+}
+
+
+
+
+
+
+
+
+// getall courses handler function
